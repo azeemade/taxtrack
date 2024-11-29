a923b4773180ce3931f5225fd747c72bb5b0198e
Azeem Adenuga <adenugaazeem80@gmail.com>
2024-11-29 06:40:49 +0100

    feature(super_admin-setup): project setup including controllers and route for super admin branch.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -11,6 +12,12 @@ return Application::configure(basePath: dirname(__DIR__))
         api: __DIR__ . '/../routes/api.php',
         commands: __DIR__ . '/../routes/console.php',
         health: '/up',
+        then: function () {
+            Route::middleware(['auth:api', 'permission:access_admin_app,api'])
+                ->prefix('admin')
+                ->namespace('Admin')
+                ->group(base_path('routes/admin/api.php'));
+        }
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
