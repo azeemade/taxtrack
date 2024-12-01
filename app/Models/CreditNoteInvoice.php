b86c9a0a841a9d346666fa346933f58468830b0b
Azeem Adenuga <adenugaazeem80@gmail.com>
2024-12-01 19:16:50 +0100

    features/sales/credit-notes: Implementation of credit note creation and view

--- app/Models/CreditNoteInvoice.php
+++ app/Models/CreditNoteInvoice.php
@@ -2,10 +2,41 @@
 
 namespace App\Models;
 
+use App\Models\Scopes\ModelUserScope;
+use App\Traits\Companyable;
+use Illuminate\Database\Eloquent\Attributes\ScopedBy;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+#[ScopedBy([ModelUserScope::class])]
 class CreditNoteInvoice extends Model
 {
-    use HasFactory;
+    use HasFactory, Companyable;
+
+    protected $guarded = ['id'];
+
+    public function createdBy()
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+
+    public function creditNote()
+    {
+        return $this->belongsTo(CreditNote::class);
+    }
+
+    public function invoice()
+    {
+        return $this->belongsTo(Invoice::class);
+    }
+
+    public function lineItem()
+    {
+        return $this->belongsTo(LineItem::class);
+    }
+
+    public function company()
+    {
+        return $this->belongsTo(Company::class);
+    }
 }
