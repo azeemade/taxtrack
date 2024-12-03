95bcf264428f3805015e02dd5bc159146e99c71c
Azeem Adenuga <adenugaazeem80@gmail.com>
2024-12-03 23:36:48 +0100

    feature/super_admin-setup: Models and db tables setup

---
+++ database/migrations/2024_12_03_221324_create_subscription_cancellations_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('subscription_cancellations', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->date('request_date');
+            $table->date('effective_from');
+            $table->string('status')->comment('pending, active');
+            $table->string('reason');
+            $table->string('additionalInformation')->nullable();
+            $table->unsignedBigInteger('subscriber_id');
+            $table->unsignedBigInteger('company_id');
+            $table->unsignedBigInteger('subscription_plan_id');
+            $table->foreign('company_id')->references('id')->on('companies')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('subscriber_id')->references('id')->on('subscribers')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('subscription_plan_id')->references('id')->on('subscription_plans')->onDelete('cascade')->onUpdate('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('subscription_cancellations');
+    }
+};
