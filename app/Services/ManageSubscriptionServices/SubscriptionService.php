12348fa4b15de5e379981d01eeb000450c699f6a
Aisimiyu Oluwadarasimi <aisimiyuoluwadara@gmail.com>
2024-12-08 19:55:03 +0100

    admin subscription start

--- app/Services/ManageSubscriptionServices/SubscriptionService.php
+++ app/Services/ManageSubscriptionServices/SubscriptionService.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Services\ManageSubscriptionServices;
+namespace App\Services\AdminUserServices;
 
 use App\Enums\GeneralEnums;
 use App\Exceptions\BadRequestException;
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Str;
 
-class SubscriptionService
+class UserService
 {
 
     protected RoleService $roleService;
