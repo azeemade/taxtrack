103e0ea014ac7f5ebb3bf93a5b61233b34e1383c
Azeem Adenuga <adenugaazeem80@gmail.com>
2025-03-02 23:42:59 +0100

    Enhance PaymentRecordService to support payment status updates and constrain recordable relationships for PurchaseInvoice and VendorBill in queries. Add payment_status field to purchase_invoices table for better tracking.

--- app/Services/PaymentRecords/PaymentRecordService.php
+++ app/Services/PaymentRecords/PaymentRecordService.php
@@ -11,7 +11,9 @@ use App\Models\BankAccount;
 use App\Models\CardAccount;
 use App\Models\PaymentMethod;
 use App\Models\PaymentRecord;
+use App\Models\PurchaseInvoice;
 use App\Models\Vendor;
+use App\Models\VendorBill;
 use App\Services\SharedServices\SharedActionService;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
@@ -37,7 +39,7 @@ class PaymentRecordService
             ]
         );
 
-        $amountDue = $record->recordable->amount_due;
+        $amountDue = $record->recordable->amount_due ??  0.00;
         $record->recordable()->update([
             'payment_status' => $amountDue > 0 ? PaymentStatusEnums::PARTIAL_PAYMENT->value : PaymentStatusEnums::FULL_PAYMENT->value
         ]);
@@ -95,11 +97,20 @@ class PaymentRecordService
                 'additional_notes',
                 'referenceID'
             ])
-            ->with([
-                'recordable:id,vendor_id,vendor_billID,share_status' => [
-                    'vendor:id,vendor_name,referenceID'
-                ]
-            ])
+            ->with(
+                ['recordable' => function (MorphTo $morphTo) {
+                    $morphTo->constrain([
+                        PurchaseInvoice::class => function ($subquery) {
+                            $subquery->select('id', 'vendor_id', 'purchase_invoiceID', 'share_status')
+                                ->with(['vendor:id,vendor_name,referenceID']);
+                        },
+                        VendorBill::class => function ($subquery) {
+                            $subquery->select('id', 'vendor_id', 'vendor_billID', 'share_status')
+                                ->with(['vendor:id,vendor_name,referenceID']);
+                        },
+                    ]);
+                }]
+            )
             ->when($request->sort_by, function ($query) use ($request) {
                 switch ($request->sort_by) {
                     case 'alphabetically':
