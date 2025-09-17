bde40824b3a94acb329fcd731774c016df7688b0
Azeem Adenuga <adenugaazeem80@gmail.com>
2025-09-17 07:10:49 +0100

    fix: update login response to include subscription details for the authenticated user

--- app/Http/Controllers/v1/Auth/AuthController.php
+++ app/Http/Controllers/v1/Auth/AuthController.php
@@ -52,9 +52,9 @@ class AuthController extends Controller
                 'last_login' => now()
             ]);
 
-            $user['companies'] = $user->getCurrentSubscriptionDetails();
 
             if ($user->current_company_id) {
+                $user['subscription'] = $user->getCurrentSubscriptionDetails();
                 $user['company']['current_currency'] = $user->company->currentCurrency();
             }
 
