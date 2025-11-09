a69f9ad5a4f73463d2efb64dafea2c2487a3b57c
Azeem Adenuga <adenugaazeem80@gmail.com>
2025-11-09 18:34:14 +0100

    fix(QuoteService): correct category name comparison in line_items mapping

--- app/Services/Quotes/QuoteService.php
+++ app/Services/Quotes/QuoteService.php
@@ -232,7 +232,7 @@ class QuoteService
         }
 
         $record['line_items'] = $record->lineItems->map(function ($item) {
-            $item['category_id'] = $item->category->name === "sales" ? $item->category_id : $item->category->name;
+            $item['category_id'] = $item->category->name === "Sales" ? $item->category_id : $item->category->name;
             return $item;
         });
         return $record;
